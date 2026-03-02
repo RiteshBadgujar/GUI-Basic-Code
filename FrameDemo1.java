08c8a5b4647fab42723cabf2fe51821a5f9f935e
RiteshBadgujar <riteshbadgujar0@gmail.com>
2026-03-02 21:26:13 +0530

    [ADDED]:Ritesh -:The create frame using awt program.

---
+++ FrameDemo1.java
@@ -0,0 +1,15 @@
+import java.awt.Frame;
+class FrameDemo1
+{
+	public static void main(String args[])
+	{
+		Frame frame = new Frame();
+
+		frame.setSize(500,500);
+		frame.setVisible(true);
+		frame.setLocation(200,250);
+		frame.setTitle("This is Frame");
+
+	}
+
+}
